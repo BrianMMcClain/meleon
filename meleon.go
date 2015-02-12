b5739e0ec5245f0d29737d3542d8c3a13ab02192
Brian McClain <brianmmcclain@gmail.com>
2015-02-12 07:31:00 -0800

    go fmt

--- meleon.go
+++ meleon.go
@@ -1,183 +1,183 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "io/ioutil"
-  "os"
-  "encoding/json"
-  "time"
-) 
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
 
 type Config struct {
-  RemoteHost string
-  LogBody bool
+	RemoteHost string
+	LogBody    bool
 }
 
 type Transaction struct {
-  Timestamp time.Time
-  Request *http.Request
-  Response *http.Response
+	Timestamp time.Time
+	Request   *http.Request
+	Response  *http.Response
 }
 
 func main() {
-  c := readConfig()
+	c := readConfig()
 
-  transactionLog := make([]Transaction, 0)
+	transactionLog := make([]Transaction, 0)
 
-  // We are an invisible proxy, so we will handle all requests the same way:
-  // Read the request from the client, record it, and pass it along as intended
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    r.Host = c.RemoteHost
-    transaction := proxyRequest(w, r, c)
-    transactionLog = append(transactionLog, transaction)
-    // b, _ := json.Marshal(transactionLog)
-    // fmt.Println(string(b))
-  })
-  http.ListenAndServe(":9999", nil)
+	// We are an invisible proxy, so we will handle all requests the same way:
+	// Read the request from the client, record it, and pass it along as intended
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r.Host = c.RemoteHost
+		transaction := proxyRequest(w, r, c)
+		transactionLog = append(transactionLog, transaction)
+		// b, _ := json.Marshal(transactionLog)
+		// fmt.Println(string(b))
+	})
+	http.ListenAndServe(":9999", nil)
 }
 
 func readConfig() Config {
-  file, err := os.Open("meleon.json")
-  if err != nil {
-    panic(err)
-  }
+	file, err := os.Open("meleon.json")
+	if err != nil {
+		panic(err)
+	}
 
-  decoder := json.NewDecoder(file)
-  config := Config{}
-  err = decoder.Decode(&config)
-  if err != nil {
-    panic(err)
-  }
+	decoder := json.NewDecoder(file)
+	config := Config{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		panic(err)
+	}
 
-  return config
+	return config
 }
 
 func proxyRequest(w http.ResponseWriter, r *http.Request, c Config) Transaction {
-  recordRequest(r, c)
-  switch r.Method {
-    case "GET":
-      resp := handleGET(w, r, c)
-      return recordTransaction(r, resp)
-    case "POST":
-      resp := handlePOST(w, r, c)
-      return recordTransaction(r, resp)
-  }
+	recordRequest(r, c)
+	switch r.Method {
+	case "GET":
+		resp := handleGET(w, r, c)
+		return recordTransaction(r, resp)
+	case "POST":
+		resp := handlePOST(w, r, c)
+		return recordTransaction(r, resp)
+	}
 
-  return Transaction{time.Now(), nil, nil}
+	return Transaction{time.Now(), nil, nil}
 }
 
 func recordTransaction(req *http.Request, resp *http.Response) Transaction {
-  t := Transaction{time.Now(), req, resp}
-  return t
+	t := Transaction{time.Now(), req, resp}
+	return t
 }
 
 // Record the method, headers, address and potentially the
 // body of the reqest
 func recordRequest(r *http.Request, c Config) {
-  fmt.Println("--------------------------------------------------------------------")
-  fmt.Printf("> %s %s\n", r.Method, r.RequestURI)
-  for k, _ := range r.Header {
-    fmt.Printf("> %s: %s\n", k, r.Header[k])
-  }
+	fmt.Println("--------------------------------------------------------------------")
+	fmt.Printf("> %s %s\n", r.Method, r.RequestURI)
+	for k, _ := range r.Header {
+		fmt.Printf("> %s: %s\n", k, r.Header[k])
+	}
 
-  if c.LogBody {
-    switch r.Method {
-    case "POST":
-      recordRequestBody(r)
-    }
-  }
+	if c.LogBody {
+		switch r.Method {
+		case "POST":
+			recordRequestBody(r)
+		}
+	}
 
-  fmt.Println()
+	fmt.Println()
 }
 
 // Read the request body and record it
 func recordRequestBody(r *http.Request) {
-  body, err := ioutil.ReadAll(r.Body)
-  if err != nil {
-    panic(fmt.Sprintf("%v", err))
-    return
-  }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+		return
+	}
 
-  fmt.Printf("\n%v\n", string(body))
+	fmt.Printf("\n%v\n", string(body))
 }
 
 func recordResponse(r *http.Response, body string, c Config) {
-  fmt.Printf("< %s\n", r.Status)
-  for k, _ := range r.Header {
-    fmt.Printf("< %s: %s\n", k, r.Header[k])
-  }
+	fmt.Printf("< %s\n", r.Status)
+	for k, _ := range r.Header {
+		fmt.Printf("< %s: %s\n", k, r.Header[k])
+	}
 
-  if c.LogBody {
-    fmt.Printf("\n%s\n", body)
-  }
-  fmt.Println()
+	if c.LogBody {
+		fmt.Printf("\n%s\n", body)
+	}
+	fmt.Println()
 }
 
 // GET requests are extremely straight-forward. We will receive the request,
 // do all processing that we need to on our side, and pass the request along
 // without modification
 func handleGET(w http.ResponseWriter, r *http.Request, c Config) *http.Response {
-  reqURL := fmt.Sprintf("%s%s", c.RemoteHost, r.RequestURI)
-  resp, err := http.Get(reqURL)
-  if err != nil {
-    panic(fmt.Sprintf("%v", err))
-    return nil
-  }
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    panic(fmt.Sprintf("%v", err))
-    return nil
-  }
+	reqURL := fmt.Sprintf("%s%s", c.RemoteHost, r.RequestURI)
+	resp, err := http.Get(reqURL)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+		return nil
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+		return nil
+	}
 
-  recordResponse(resp, string(body), c)
+	recordResponse(resp, string(body), c)
 
-  // Copy headers
-  cRespHeader := w.Header()
-  for k, _ := range resp.Header {
-    cRespHeader[k] = resp.Header[k]
-  }
+	// Copy headers
+	cRespHeader := w.Header()
+	for k, _ := range resp.Header {
+		cRespHeader[k] = resp.Header[k]
+	}
 
-  // Set status code
-  w.WriteHeader(resp.StatusCode)
+	// Set status code
+	w.WriteHeader(resp.StatusCode)
 
-  // Send the response back to the client
-  w.Write(body)
+	// Send the response back to the client
+	w.Write(body)
 
-  return resp
+	return resp
 }
 
 // POST requests only have the extra step of ensuring that we read the POST
 // body from the request and pass it along as intended, unmodified
 func handlePOST(w http.ResponseWriter, r *http.Request, c Config) *http.Response {
-  reqURL := fmt.Sprintf("%s%s", c.RemoteHost, r.RequestURI)
-  // TODO: Read the request body and send it in the request
-  resp, err := http.Post(reqURL, "", nil)
-  if err != nil {
-    panic(fmt.Sprintf("%v", err))
-    return nil
-  }
-  defer resp.Body.Close()
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    panic(fmt.Sprintf("%v", err))
-    return nil
-  }
+	reqURL := fmt.Sprintf("%s%s", c.RemoteHost, r.RequestURI)
+	// TODO: Read the request body and send it in the request
+	resp, err := http.Post(reqURL, "", nil)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+		return nil
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+		return nil
+	}
 
-  recordResponse(resp, string(body), c)
+	recordResponse(resp, string(body), c)
 
-  // Copy headers
-  cRespHeader := w.Header()
-  for k, _ := range resp.Header {
-    cRespHeader[k] = resp.Header[k]
-  }
+	// Copy headers
+	cRespHeader := w.Header()
+	for k, _ := range resp.Header {
+		cRespHeader[k] = resp.Header[k]
+	}
 
-  // Set status code
-  w.WriteHeader(resp.StatusCode)
+	// Set status code
+	w.WriteHeader(resp.StatusCode)
 
-  // Send the response back to the client
-  w.Write(body)
+	// Send the response back to the client
+	w.Write(body)
 
-  return resp
+	return resp
 }
